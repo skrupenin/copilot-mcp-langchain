31de0a1f83f2e7926fbf72d9c16bd748d7d9dfe5
Oleksandr Baglai <Oleksandr_Baglai@epam.com>
2025-06-12 11:57:02 +0300

    Added templates

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
 
 OPENAI_API_KEY = "***REMOVED***"
 llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 
-prompt = "Привет, как ты себя чувствуешь сегодня?"
+template = """
+Привет, {name}, как ты себя чувствуешь сегодня?
+"""
+prompt_template = PromptTemplate(
+    input_variables=["name"],  
+    template=template
+)
+
+name = "Саша"
+prompt = prompt_template.format(name=name)
 
 response = llm(prompt)
 print(response)
